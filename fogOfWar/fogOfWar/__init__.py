f42e7e1ace44a0c71f953476087b807005dc4836
tcdavis <TheDavis@users.noreply.github.com>
2020-01-26 22:54:21 -0600

    added image preview

--- fogOfWar/fogOfWar/__init__.py
+++ fogOfWar/fogOfWar/__init__.py
@@ -3,6 +3,14 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_socketio import SocketIO
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'HOME NOOK CANTER HILL'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+    socketio.run(app)
 
 import fogOfWar.views
+
